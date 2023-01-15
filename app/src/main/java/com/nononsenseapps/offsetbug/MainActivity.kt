7948c6c82d2172df96e211f014c5330b225c6630
Jonas Kalderstam <jonas@cowboyprogrammer.org>
2023-01-15 11:42:00 +0100

    Bug introduced with drawer

--- app/src/main/java/com/nononsenseapps/offsetbug/MainActivity.kt
+++ app/src/main/java/com/nononsenseapps/offsetbug/MainActivity.kt
@@ -3,61 +3,103 @@ package com.nononsenseapps.offsetbug
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DismissibleDrawerSheet
+import androidx.compose.material3.DismissibleNavigationDrawer
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 import com.nononsenseapps.offsetbug.ui.theme.OffsetBugTheme
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             OffsetBugTheme {
-                Scaffold(
-                    topBar = {
-                        TopAppBar(
-                            title = {
-                                Text("OffsetBug")
-                            },
-                            actions = {
-                                IconButton(onClick = { /*TODO*/ }) {
-                                    Icon(Icons.Default.MoreVert, contentDescription = "Action")
+                val coroutineScope = rememberCoroutineScope()
+                val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+
+                DismissibleNavigationDrawer(
+                    drawerState = drawerState,
+                    drawerContent = {
+                        DismissibleDrawerSheet {
+                            Text("Drawer example")
+                        }
+                    }
+                ) {
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(
+                                title = {
+                                    Text("OffsetBug")
+                                },
+                                actions = {
+                                    IconButton(onClick = { /*TODO*/ }) {
+                                        Icon(Icons.Default.MoreVert, contentDescription = "Action")
+                                    }
+                                },
+                                navigationIcon = {
+                                    IconButton(onClick = {
+                                        coroutineScope.launch {
+                                            drawerState.open()
+                                        }
+                                    }) {
+                                        Icon(Icons.Default.Menu, contentDescription = "Menu")
+                                    }
                                 }
-                            },
-                            navigationIcon = {
-                                IconButton(onClick = { /*TODO*/ }) {
-                                    Icon(Icons.Default.Menu, contentDescription = "Menu")
-                                }
-                            }
-                        )
-                    },
-                    modifier = Modifier
-                ) { paddingValues ->
-//                    Box(
-//                        contentAlignment = Alignment.Center,
-//                        modififer = Modifier
-//                            .padding(paddingValues)
-//                            .fillMaxSize(),
-//                    ) {
-                    Text(
-                        text = loremIpsum,
+                            )
+                        },
                         modifier = Modifier
-                            .padding(paddingValues)
-                            .padding(horizontal = 8.dp)
-                    )
-//                    }
+                            .windowInsetsPadding(WindowInsets.navigationBars.only(WindowInsetsSides.Horizontal)),
+//                        .windowInsetsPadding(WindowInsets.navigationBars.only(WindowInsetsSides.Horizontal)),
+                        contentWindowInsets = WindowInsets.statusBars,
+                    ) { paddingValues ->
+                        val scrollState = rememberScrollState()
+
+                        Box(
+                            contentAlignment = Alignment.Center,
+                            modifier = Modifier
+                                .padding(paddingValues)
+                                .padding(horizontal = 8.dp)
+                                .fillMaxSize()
+                                .verticalScroll(state = scrollState),
+                        ) {
+                            Text(
+                                text = loremIpsum,
+                            )
+                        }
+                    }
+
                 }
             }
         }
